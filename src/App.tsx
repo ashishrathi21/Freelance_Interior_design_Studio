b86fcfe7cf93392b09721a7c652664fa364def99
Ashish Rathi <rathiashish4321@gmail.com>
2025-11-15 10:34:41 +0530

    modified:   src/App.tsx 	modified:   src/index.css

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import  Navbar  from './components/Navbar';
 
 function App() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen w-full max-w-full overflow-x-hidden">
       <Navbar/>
       <Hero />
       <About />
